2fdc0da84a62c6cac7333e5b7bdc567feeb44a5b
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-05-04 13:11:34 +0530

    add unsubscribe

--- skills/subscribe.js
+++ skills/subscribe.js
@@ -28,36 +28,53 @@ module.exports = function (controller) {
         })
       })
 
-      controller.hears('unsubscribe', 'direct_mention', function (bot, message) {
-
-        controller.storage.channels.get(message.channel, function (err, channel) {
-          if (!channel || !channel.subscribed) {
-            channel = {}
-            channel.id = message.channel
-            channel.subscribed = []
-          }
-          if (channel.subscribed.includes(message.user)) {
-            channel.subscribed.splice(channel.subscribed.indexOf(message.user),1)
-          }
-    
-          MongoClient.connect(url, (err, client) => {
-            const db = client.db('test')
-    
-            db.collection('local', null, (err, col) => {
-              col.updateOne({subscribed: channel.subscribed},
-                {$set: {subscribed: channel.subscribed}},
-                {upsert: true}
-              )
-              col.find({}).toArray((e, items) => console.log(items))
-              client.close()
-            })
-          })
-    
-
       controller.storage.channels.save(channel, function (err, saved) {
 
         if (err) {
           bot.reply(message, 'I experienced an error adding you :' + err)
+        } else {
+          bot.api.reactions.add({
+            name: 'thumbsup',
+            channel: message.channel,
+            timestamp: message.ts
+          })
+        }
+
+      })
+    })
+
+  })
+
+
+  controller.hears('unsubscribe', 'direct_mention', function (bot, message) {
+
+    controller.storage.channels.get(message.channel, function (err, channel) {
+      if (!channel || !channel.subscribed) {
+        channel = {}
+        channel.id = message.channel
+        channel.subscribed = []
+      }
+      if (channel.subscribed.includes(message.user)) {
+        channel.subscribed.splice(channel.indexOf(message.user), 1)
+      }
+
+      MongoClient.connect(url, (err, client) => {
+        const db = client.db('test')
+
+        db.collection('local', null, (err, col) => {
+          col.updateOne({subscribed: channel.subscribed},
+            {$set: {subscribed: channel.subscribed}},
+            {upsert: true}
+          )
+          col.find({}).toArray((e, items) => console.log(items))
+          client.close()
+        })
+      })
+
+      controller.storage.channels.save(channel, function (err, saved) {
+
+        if (err) {
+          bot.reply(message, 'I experienced an error removing you :' + err)
         } else {
           bot.api.reactions.add({
             name: 'thumbsup',
