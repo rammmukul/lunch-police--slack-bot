b533a4ae9b13000bffbb64d1bca52abf0f5e1186
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-05-04 07:56:52 +0530

    t

--- skills/subscribe.js
+++ skills/subscribe.js
@@ -8,10 +8,11 @@ module.exports = function (controller) {
       if (!channel) {
         channel = {}
         channel.id = message.channel
-        channel.subscribed = new Set()
+        channel.subscribed = []
       }
-
-      channel.subscribed.add(message.user)
+      if (!channel.subscribed.includes(message.user)) {
+        channel.subscribed.push(message.user)
+      }
 
       controller.storage.channels.save(channel, function (err, saved) {
 
@@ -36,30 +37,6 @@ module.exports = function (controller) {
     controller.storage.channels.get(message.channel, function (err, channel) {
 
       bot.reply(message, channel.subscribed.join(','))
-    })
-
-  })
-
-
-  controller.hears('clear', 'direct_mention', function (bot, message) {
-
-    controller.storage.channels.get(message.channel, function (err, channel) {
-
-      channel.subscribed = undefined
-
-      controller.storage.channels.save(channel, function (err, saved) {
-
-        if (err) {
-          bot.reply(message, 'I experienced an error adding you :' + err)
-        } else {
-          bot.api.reactions.add({
-            name: 'thumbsup',
-            channel: message.channel,
-            timestamp: message.ts
-          })
-        }
-
-      })
     })
 
   })
