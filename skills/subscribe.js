f820e94b3c90fd64bf2d7839154b86544b676d73
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-05-04 07:26:48 +0530

    t

---
+++ skills/subscribe.js
@@ -0,0 +1,30 @@
+
+module.exports = function (controller) {
+
+  controller.hears('subscribe', ['direct_message', 'direct_mention'], function (bot, message) {
+
+    controller.storage.channels.get(message.channel, function (err, user) {
+
+      if (!channel.subscribed) {
+        channel.subscribed = []
+      }
+
+      channel.subscribed.push(message.user)
+
+      controller.storage.channels.save(message.channel, function (err, saved) {
+
+        if (err) {
+          bot.reply(message, 'I experienced an error adding you :' + err);
+        } else {
+          bot.api.reactions.add({
+            name: 'thumbsup',
+            channel: message.channel,
+            timestamp: message.ts
+          })
+        }
+
+      })
+    })
+
+  })
+}
