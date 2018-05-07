536de122c19c0d1d7fc261321e19cb8c5c3e7ae5
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-05-07 14:06:59 +0530

    t

--- tasks/scheduel.js
+++ tasks/scheduel.js
@@ -1,1 +1,3 @@
 console.log('<><><><><><><>run<><><><><><><>')
+setInterval(() => console.log('<><><><><><><>scheduled<><><><><><><>'), 1860000)
+setInterval(() => console.log('<><><><><><><>tick<><><><><><><>'), 60000)
