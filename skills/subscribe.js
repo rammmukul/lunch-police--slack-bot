0322e80cf440c34003ce5fc575bfad3df0ca3d70
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-05-15 11:11:18 +0530

    t

--- skills/subscribe.js
+++ skills/subscribe.js
@@ -10,16 +10,15 @@ module.exports = function (controller) {
       const db = client.db('test')
       let col = await db.collection('lunch')
       let subscribed = await col.find({ _id: 'lunch' }).toArray()[0]
+
       subscribed = subscribed ? subscribed.subscribed : []
       if (!subscribed.includes(message.user)) {
         subscribed.push(message.user)
       }
-      console.log('<><><><>', subscribed, '<><><><>')
       col.updateOne({ _id: 'lunch' },
         { $set: { _id: 'lunch', subscribed: subscribed } },
         { upsert: true }
       )
-      col.find({}).toArray((e, items) => console.log('>>>>',items))
       client.close()
 
       bot.api.reactions.add({
@@ -35,52 +34,47 @@ module.exports = function (controller) {
   })
 
 
-  controller.hears('^\s*unsubscribe\s+lunch', 'direct_mention', function (bot, message) {
+  controller.hears('^\s*unsubscribe\s+lunch', 'direct_mention', async function (bot, message) {
 
-    controller.storage.channels.get(message.channel, function (err, channel) {
-      if (!channel || !channel.subscribed) {
-        channel = {}
-        channel.id = message.channel
-        channel.subscribed = []
+    try {
+      let client = await MongoClient.connect(url)
+      const db = client.db('test')
+      let col = await db.collection('lunch')
+      let subscribed = await col.find({ _id: 'lunch' }).toArray()[0]
+
+      subscribed = subscribed ? subscribed.subscribed : []
+      if (subscribed.includes(message.user)) {
+        subscribed.splice(subscribed.indexOf(message.user), 1)
       }
-      if (channel.subscribed.includes(message.user)) {
-        channel.subscribed.splice(channel.subscribed.indexOf(message.user), 1)
-      }
+      col.updateOne({ _id: 'lunch' },
+        { $set: { _id: 'lunch', subscribed: subscribed } },
+        { upsert: true }
+      )
+      client.close()
 
-      MongoClient.connect(url, (err, client) => {
-        const db = client.db('test')
-
-        db.collection('lunch', null, (err, col) => {
-          col.updateOne({ _id: channel.id },
-            { $set: { _id: channel.id, subscribed: channel.subscribed } },
-            { upsert: true }
-          )
-          col.find({}).toArray((e, items) => console.log(items))
-          client.close()
-        })
+      bot.api.reactions.add({
+        name: 'thumbsup',
+        channel: message.channel,
+        timestamp: message.ts
       })
 
-      controller.storage.channels.save(channel, function (err, saved) {
-
-        if (err) {
-          bot.reply(message, 'I experienced an error removing you :' + err)
-        } else {
-          bot.api.reactions.add({
-            name: 'thumbsup',
-            channel: message.channel,
-            timestamp: message.ts
-          })
-        }
-
-      })
-    })
-
+    } catch (err) {
+      bot.reply(message, 'I experienced an error removing you :' + err)
+    }
   })
 
-  controller.hears('^\s*show\s+lunch', 'direct_mention', function (bot, message) {
-    controller.storage.channels.get(message.channel, function (err, channel) {
-      bot.reply(message, channel.subscribed.map(user => '<@' + user + '>').join(','))
-    })
+  controller.hears('^\s*show\s+lunch', 'direct_mention', async function (bot, message) {
+    try {
+      let client = await MongoClient.connect(url)
+      const db = client.db('test')
+      let col = await db.collection('lunch')
+      let subscribed = await col.find({ _id: 'lunch' }).toArray()[0]
 
+      subscribed = subscribed ? subscribed.subscribed : []
+      bot.reply(message, subscribed.map(user => '<@' + user + '>').join(','))
+      client.close()
+    } catch (err) {
+      bot.reply(message, 'I experienced an error showing subscriptions :' + err)
+    }
   })
 }
