dd9af82c8f4e935bfa22b5538cd11e6544e2c2cc
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-05-15 12:38:33 +0530

    t

--- skills/subscribe.js
+++ skills/subscribe.js
@@ -80,8 +80,35 @@ module.exports = function (controller) {
 
 
   controller.hears('add to lunch <@.*>', 'direct_mention', async function (bot, message) {
-    let reg = /<@((?:\d|\w)*)>/g
-    console.log('<<<<', message.text, JSON.stringify(reg.exec(message.text), null, 2))
-    bot.reply(message, 'Hmm...')
+    let regx = /<@((?:\d|\w)*)>/g
+    console.log('<<<<', message.text, JSON.stringify(regx.exec(message.text), null, 2))
+    let add = message.text.match(regx).map(user => user.slice(2, -1))
+    try {
+      let client = await MongoClient.connect(url)
+      const db = client.db('test')
+      let col = await db.collection('lunch')
+      let subscribed = await col.find({ _id: 'lunch' }).toArray()[0]
+
+      subscribed = subscribed ? subscribed.subscribed : []
+      add.forEach(user => {
+        if (!subscribed.includes(user)) {
+          subscribed.push(user)
+        }        
+      })
+      col.updateOne({ _id: 'lunch' },
+        { $set: { _id: 'lunch', subscribed: subscribed } },
+        { upsert: true }
+      )
+      client.close()
+
+      bot.api.reactions.add({
+        name: 'thumbsup',
+        channel: message.channel,
+        timestamp: message.ts
+      })
+
+    } catch (err) {
+      bot.reply(message, 'I experienced an error adding :' + err)
+    }
   })
 }
