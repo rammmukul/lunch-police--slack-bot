9207d74371e31218a09121c4d30feb9f40a32863
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-05-15 16:13:55 +0530

    t

--- bot.js
+++ bot.js
@@ -105,4 +105,4 @@ if (!process.env.clientId || !process.env.clientSecret) {
     console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/')
   }
 }
-console.log('main file', this)
+console.log('main file', bot)
