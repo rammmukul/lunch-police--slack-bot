d5f8d3be5b458488b37263fc2a0ffc02385b738d
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-05-15 09:34:03 +0530

    remove clock

--- tasks/scheduel.js
+++
@@ -1,8 +0,0 @@
-console.log('<><><><><><><>run<><><><><><><>')
-setInterval(() => console.log('<><><><><><><>scheduled<><><><><><><>'), 1860000)
-setInterval(ticker, 60000)
-
-var count = 0
-function ticker() {
-    console.log('<><><><><><><>'+ count++ +'<><><><><><><>')
-}
