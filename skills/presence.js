cce925feeef027c72ab3ccc2a40550a14b5a0173
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-05-16 10:23:20 +0530

    t

--- skills/presence.js
+++ skills/presence.js
@@ -5,6 +5,7 @@ module.exports = function (controller) {
 
   controller.hears('.*', 'ambient', async function (bot, message) {
     console.log('message: ' + message.user)
+    console.log(':::::::: ' + JSON.stringify(message, null, 2))
   })
 
 }
