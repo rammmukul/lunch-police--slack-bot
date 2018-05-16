08245ca74c53171b7f5d85e599b7bb9db97b3d8c
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-05-16 11:24:13 +0530

    t

--- skills/presence.js
+++ skills/presence.js
@@ -1,12 +1,13 @@
 const MongoClient = require('mongodb').MongoClient
 const url = process.env.MONGO_URI
-let moment =require('moment')
 
 module.exports = function (controller) {
 
   controller.hears('.*', 'ambient', async function (bot, message) {
     console.log('message: ' + message.user)
     console.log(':::::::: ' + JSON.stringify(message, null, 2))
+
+    let moment = require('moment')
     console.log(moment.startOf('day').format('YYYY MM DD'))
   })
 
