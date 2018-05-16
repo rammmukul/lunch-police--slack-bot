adb0646b953c5e99860365193424448cb28249a3
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-05-16 12:42:14 +0530

    t

--- skills/presence.js
+++ skills/presence.js
@@ -11,71 +11,76 @@ module.exports = function (controller) {
     let today = moment().startOf('day').format('DD MM YYYY')
 
     try {
-        let client = await MongoClient.connect(url)
-        const db = client.db('test')
-        let col = await db.collection('presence')
-        let monitor = (await col.find({ _id: 'group' }).toArray())[0].monitor
-        if (message.channel !== monitor) return
+      let client = await MongoClient.connect(url)
+      const db = client.db('test')
+      let col = await db.collection('presence')
+      let monitor = (await col.find({ _id: 'group' }).toArray())[0].monitor
+      if (message.channel !== monitor) return
 
-        let presence = (await col.find({ _id: today }).toArray())[0]
-        presence = presence ? presence.presence : []
-        if (!presence.includes(message.user)) {
-          presence.push(message.user)
-        }
-        col.updateOne({ _id: today },
-          { $set: { _id: today, presence: presence } },
-          { upsert: true }
-        )
-        client.close()
-  
-        bot.api.reactions.add({
-          name: 'heavy_check_mark',
-          channel: message.channel,
-          timestamp: message.ts
-        })
-  
-      } catch (err) {
-        console.log(err)
+      let presence = (await col.find({ _id: today }).toArray())[0]
+      presence = presence ? presence.presence : []
+      if (!presence.includes(message.user)) {
+        presence.push(message.user)
       }
+      col.updateOne({ _id: today },
+        { $set: { _id: today, presence: presence } },
+        { upsert: true }
+      )
+      client.close()
+
+      bot.api.reactions.add({
+        name: 'heavy_check_mark',
+        channel: message.channel,
+        timestamp: message.ts
+      })
+
+    } catch (err) {
+      console.log(err)
+    }
   })
 
-  controller.hears('^\s*monitor presence', 'direct_mention', async function(bot, message) {
+  controller.hears('^\s*monitor presence', 'direct_mention', async function (bot, message) {
     try {
-        let client = await MongoClient.connect(url)
-        const db = client.db('test')
-        let col = await db.collection('presence')
-        
-        col.updateOne({ _id: 'group' },
-          { $set: { _id: 'group', monitor: message.channel } },
-          { upsert: true }
-        )
-        client.close()
-  
-        bot.api.reactions.add({
-          name: 'thumbsup',
-          channel: message.channel,
-          timestamp: message.ts
-        })
-  
-      } catch (err) {
-        bot.reply(message, 'I experienced an error saving configuration :' + err)
-      }
+      let client = await MongoClient.connect(url)
+      const db = client.db('test')
+      let col = await db.collection('presence')
+
+      col.updateOne({ _id: 'group' },
+        { $set: { _id: 'group', monitor: message.channel } },
+        { upsert: true }
+      )
+      client.close()
+
+      bot.api.reactions.add({
+        name: 'thumbsup',
+        channel: message.channel,
+        timestamp: message.ts
+      })
+
+    } catch (err) {
+      bot.reply(message, 'I experienced an error saving configuration :' + err)
+    }
   })
 
   controller.hears('^\s*show presence', 'direct_mention', async function (bot, message) {
     try {
-        let client = await MongoClient.connect(url)
-        const db = client.db('test')
-        let col = await db.collection('presence')
-        let monitor = (await col.find({ _id: 'group' }).toArray())[0].monitor
-        if (message.channel !== monitor) return
+      let client = await MongoClient.connect(url)
+      const db = client.db('test')
+      let col = await db.collection('presence')
+      let monitor = (await col.find({ _id: 'group' }).toArray())[0].monitor
+      if (message.channel !== monitor) return
 
-        let presence = (await col.find({}).toArray())
-        client.close()
+      let presence = (await col.find({}).toArray()).filter(obj => obj._id !== 'group')
+      client.close()
+      let attendence = presence
+        .map(obj =>
+          obj._id + ':\n' + obj.presence
+            .map(user => '<@' + user + '>'))
+        .join('\n')
 
-        bot.reply(message, "presence" + JSON.stringify(presence, null, 2))
-      } catch (err) {
-        console.log(err)
-      }
+      bot.reply(message, "presence:\n" + attendence)
+    } catch (err) {
+      console.log(err)
+    }
   })
 }
