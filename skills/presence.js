4718b36e388f6d2cbc98bde3435ec8520d573051
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-05-16 10:17:57 +0530

    t

---
+++ skills/presence.js
@@ -0,0 +1,10 @@
+const MongoClient = require('mongodb').MongoClient
+const url = process.env.MONGO_URI
+
+module.exports = function (controller) {
+
+  controller.hears('.*', 'ambient', async function (bot, message) {
+    console.log('message: ' + message.user)
+  })
+
+}
