de774a8833b38dfd320bb5076a22e27058604f07
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-05-17 16:52:11 +0530

    t

--- components/onboarding.js
+++ components/onboarding.js
@@ -1,34 +1,22 @@
 var debug = require('debug')('botkit:onboarding');
 
-module.exports = function(controller) {
+module.exports = function (controller) {
 
-    controller.on('onboard', function(bot) {
+    controller.on('onboard', function (bot) {
 
         debug('Starting an onboarding experience!');
 
-        if (controller.config.studio_token) {
-            bot.api.im.open({user: bot.config.createdBy}, function(err, direct_message) {
-                if (err) {
-                    debug('Error sending onboarding message:', err);
-                } else {
-                    controller.studio.run(bot, 'onboarding', bot.config.createdBy, direct_message.channel.id, direct_message).catch(function(err) {
-                        debug('Error: encountered an error loading onboarding script from Botkit Studio:', err);
-                    });
-                }
-            });
-        } else {
-            bot.startPrivateConversation({user: bot.config.createdBy},function(err,convo) {
-              if (err) {
+        bot.startPrivateConversation({ user: bot.config.createdBy }, function (err, convo) {
+            if (err) {
                 console.log(err);
-              } else {
+            } else {
                 convo.say('I am a bot that has just joined your team');
                 convo.say('You must now /invite me to a channel so that I can be of use!');
                 convo.say('You can say "monitor presence" to me in a channel where you want to moniter user presence daily.');
                 convo.say('You can also say "add to lunch <user mentions>" to me I will add them to lunch duty.');
                 convo.say('when you will say "ask lunch" to me I will select one of person for the task');
-              }
-            });
-        }
+            }
+        });
     });
 
 }
