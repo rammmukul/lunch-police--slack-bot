a50406a410428795827c94a818a8776427d596ee
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-05-29 12:32:13 +0530

    t

--- skills/lunch.js
+++ skills/lunch.js
@@ -181,7 +181,8 @@ module.exports = function (controller) {
       let lunchDuty = present.lunchDuty
 
       if (lunchDuty && subscribed.includes(lunchDuty)) {
-        scheduled.reverse().push(lunchDuty).reverse()
+        scheduled = scheduled.reverse().push(lunchDuty)
+        scheduled = scheduled.reverse()
       }
       if (scheduled.includes(asked)) {
         scheduled.splice(scheduled.indexOf(asked), 1)
